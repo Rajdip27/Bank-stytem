0a4b187c315d45980aeec7fb79c2a5b99119425f
Rajdip27 <srajdip920@gamil.com>
2025-02-20 00:53:17 +0600

    ass

---
+++ BankSystem.App/Controllers/User/UserViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.App.Controllers.User
+{
+    public class UserViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
